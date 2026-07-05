b8838875294dadd838999937cadafc26ad586e90
EviL DeEdZ <sahilverma1311@protonmail.com>
2026-07-05 10:18:46 +0530

    Copy src/components/common/index.ts from myalias-id/alias-agent-prototype@dev

---
+++ src/components/common/index.ts
@@ -0,0 +1,5 @@
+export { default as AboutModal } from './about-modal';
+export { default as AudioVisualizer } from './audio-visualizer';
+export { default as LoadingIndicator } from './loading-indicator-no-progress';
+export { default as TextAreaWithButton } from './textarea-with-button';
+export { VideoBackground } from './VideoBackground';
