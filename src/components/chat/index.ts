659b89c06482e8a9d8223efc38780d394c87cab0
EviL DeEdZ <sahilverma1311@protonmail.com>
2026-07-05 10:18:30 +0530

    Copy src/components/chat/index.ts from myalias-id/alias-agent-prototype@dev

---
+++ src/components/chat/index.ts
@@ -0,0 +1,6 @@
+export { ChatMessage } from './chat-message';
+export { ChatPrivateMessageUI } from './chat-private-message-ui';
+export { ChatPublicMessageUI } from './chat-public-message-ui';
+export { ChatPublicMessageUIMobile } from './chat-public-message-ui-mobile';
+export { default as PinnedMessageBanner } from './pinned-message-banner';
+// export { ChatTexture } from './chat-texture';
