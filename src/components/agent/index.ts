788b4bfd9727434d3829005ddcab5eeb403c7f42
EviL DeEdZ <sahilverma1311@protonmail.com>
2026-07-05 10:18:18 +0530

    Copy src/components/agent/index.ts from myalias-id/alias-agent-prototype@dev

---
+++ src/components/agent/index.ts
@@ -0,0 +1,3 @@
+export { default as AgentAvatar } from './agent-avatar';
+export { default as AgentChat } from './agent-chat';
+export { default as AgentChatFullScreenCanvas } from './agent-chat-fullscreen-canvas';
