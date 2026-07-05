23d5903f032b6be5e10509c24c46d550741048dc
EviL DeEdZ <sahilverma1311@protonmail.com>
2026-07-05 10:19:28 +0530

    Copy src/instrumentation-client.ts from myalias-id/alias-agent-prototype@dev

---
+++ src/instrumentation-client.ts
@@ -0,0 +1,26 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: 'https://ef1baf5ad3d429799c1e35b13046a02f@o4509030861963264.ingest.de.sentry.io/4509030863470672',
+
+  // Add optional integrations for additional features
+  integrations: [Sentry.replayIntegration()],
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Define how likely Replay events are sampled.
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
+
+  // Define how likely Replay events are sampled when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
