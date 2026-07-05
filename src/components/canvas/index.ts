194ef798e4115dcfdc1c1351ee968c699dc11db0
EviL DeEdZ <sahilverma1311@protonmail.com>
2026-07-05 10:18:25 +0530

    Copy src/components/canvas/index.ts from myalias-id/alias-agent-prototype@dev

---
+++ src/components/canvas/index.ts
@@ -0,0 +1,6 @@
+export { Background360 } from './background-360';
+export { BackgroundChromaKey } from './background-chroma-key';
+export { BackgroundImageCanvas } from './background-image-canvas';
+export { BubbleDiv } from './bubble-div';
+export { FloatingBubbles } from './floating-bubbles';
+export { MainCanvas } from './main-canvas';
