f8efca6b40f2e6bd1030d1b5ea9b58f8b7de46be
EviL DeEdZ <sahilverma1311@protonmail.com>
2026-07-05 10:17:58 +0530

    Copy sentry.server.config.ts from myalias-id/alias-agent-prototype@dev

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: 'https://ef1baf5ad3d429799c1e35b13046a02f@o4509030861963264.ingest.de.sentry.io/4509030863470672',
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
